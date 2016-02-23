b0606b9fd023bc2cd74aa0cf0c288b86778957fd
Noam Kfir <noam@kfir.cc>
2016-02-23 11:08:25 +0200

    Added progress message to feedback view.

--- features/events/client/feedback/index/feedbackIndex.js
+++ features/events/client/feedback/index/feedbackIndex.js
@@ -1,16 +1,29 @@
+function setMessage(message, error) {
+    Session.set('feedback-message', error ? error : message);
+}
+
 Template['feedbackIndex'].events({
     'click .fetch-feedback-command': function(e) {
         e.preventDefault();
-        Meteor.call('fetchFeedback', this._id);
+        Session.set('feedback-message', 'fetching');
+        Meteor.call('fetchFeedback', this._id, function(error) {
+            setMessage('fetched', error);
+        });
     },
     'click .clear-feedback-command': function(e) {
         e.preventDefault();
-        Meteor.call('clearFeedback', this._id);
+        Session.set('feedback-message', 'clearing');
+        Meteor.call('clearFeedback', this._id, function(error) {
+            setMessage('cleared', error);
+        });
     }
 });
 
 Template['feedbackIndex'].helpers({
     getAnswerValue: function(question, answers) {
         return answers[question.id];
+    },
+    message: function () {
+        return Session.get('feedback-message');
     }
 });
